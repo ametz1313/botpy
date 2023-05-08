595f176f501b602deae35901b0e1e21293e6f5c0
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2023-05-08 15:26:38 +0000

    first commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+import telegram
+import requests
+
+# Replace YOUR_TOKEN and YOUR_CHANNEL_ID with your actual token and channel ID
+bot = telegram.Bot(token='6159945847:AAEKHNX7DoC21bUGLNU2WDBdVGImJAXZGbk')
+channel_id = '-1001482956376'
+
+# Replace URL with the URL of the file you want to read
+url = 'https://github.com/ametz1313/All/blob/main/Ssr.txt'
+
+# Make a GET request to the given URL
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Split the response into lines and send each line as a separate message to the channel
+    for line in response.text.split('\n'):
+        bot.send_message(chat_id=channel_id, text=line)
+else:
+    print('Error reading URL:', response.status_code, response.reason)
